765333e7d6ad6486f3764e996e9156d9091d6912
Antreas Kokolantonakis <antreas.kokolantonakis@gmail.com>
2020-04-09 12:29:42 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I HAVE CHANGED
+          {/* Edit <code>src/App.js</code> and save to reload. */}
         </p>
         <a
           className="App-link"
+          
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
